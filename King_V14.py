bbf6b0be1311b22c398866d65229edc74fd1d770
Benny Godwin M <bennygodwin03@gmail.com>
2026-07-06 15:51:27 -0500

    Add files via upload

--- King_V14.py
+++ King_V14.py
@@ -1604,7 +1604,7 @@ def train_model():
             gradient_steps=1,
             target_update_interval=500,
             exploration_fraction=0.7,
-            exploration_initial_eps=0.7,
+            exploration_initial_eps=0.5,
             exploration_final_eps=0.15,
             tensorboard_log="./dqn_tensorboard/"
         )
